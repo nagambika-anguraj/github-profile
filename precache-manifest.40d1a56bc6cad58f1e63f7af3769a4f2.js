10e1b18e4e97a325cad2e50e3c5d01d2b9ac2949
Nagambika Anguraj <nagambika.anguraj@gmail.com>
2019-03-07 20:44:20 -0600

    Updates

---
+++ precache-manifest.40d1a56bc6cad58f1e63f7af3769a4f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b12df915b56c2e95965",
+    "url": "/github-profile/static/js/runtime~main.0ca3beda.js"
+  },
+  {
+    "revision": "c4a7f3b8bb8f542bad5a",
+    "url": "/github-profile/static/js/main.1a07d39b.chunk.js"
+  },
+  {
+    "revision": "3b1356ad327459cbba5b",
+    "url": "/github-profile/static/js/2.452cc8f4.chunk.js"
+  },
+  {
+    "revision": "c4a7f3b8bb8f542bad5a",
+    "url": "/github-profile/static/css/main.d2559363.chunk.css"
+  },
+  {
+    "revision": "887bd20a2553720e7d182e5df7d4ffbb",
+    "url": "/github-profile/index.html"
+  }
+];
